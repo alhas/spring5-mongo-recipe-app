dfac9e7f803af5bff4e9c95a7a36f124ddd4e0f9
Bahtiyar Ali ALHAS <bahtiyaralialhas@gmail.com>
2020-03-26 20:10:07 +0100

    Refactoring data model to MongoDB

--- src/main/java/guru/springframework/domain/Ingredient.java
+++ src/main/java/guru/springframework/domain/Ingredient.java
@@ -2,20 +2,22 @@ package guru.springframework.domain;
 
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.DBRef;
+import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.math.BigDecimal;
 
-/**
- * Created by jt on 6/13/17.
- */
 @Getter
 @Setter
 public class Ingredient {
 
+    @Id
     private String id;
     private String description;
     private BigDecimal amount;
 
+    @DBRef
     private UnitOfMeasure uom;
     private Recipe recipe;
 
--- src/main/java/guru/springframework/domain/Notes.java
+++ src/main/java/guru/springframework/domain/Notes.java
@@ -2,6 +2,8 @@ package guru.springframework.domain;
 
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
 
 
 /**
@@ -11,6 +13,7 @@ import lombok.Setter;
 @Setter
 public class Notes {
 
+    @Id
     private String id;
     private Recipe recipe;
     private String recipeNotes;
